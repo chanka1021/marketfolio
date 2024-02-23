e6ad6f6d08e87fa7891940263ce689a3d262b50c
Ashraf <59064232+chanka1021@users.noreply.github.com>
2024-02-23 02:35:29 +0100

    City Selector completed

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -7,7 +7,6 @@ import {
   useDisclosure,
   Drawer,
   DrawerBody,
-  DrawerFooter,
   DrawerHeader,
   DrawerOverlay,
   DrawerContent,
@@ -18,16 +17,24 @@ import {
   AccordionPanel,
   AccordionIcon,
 } from "@chakra-ui/react";
-import { FaMapPin } from "react-icons/fa";
 import { CiSearch } from "react-icons/ci";
 import { MdNavigateNext } from "react-icons/md";
 import { Categories } from "../data/categories";
+import { FaTreeCity } from "react-icons/fa6";
+import { Cities } from "./../data/cities";
 
 function Searchbar() {
+  // State variables for selected category, filtered cities, and city selection modal
   const [selectedCategory, setSelectedCategory] = useState(Categories[0]);
+  const [filteredCities, setFilteredCities] = useState(Cities);
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const { isOpen: isOpen2, onOpen: onOpen2, onClose: onClose2 } = useDisclosure();
+  const [selectedCity, setSelectedCity] = useState("ALL");
+  // Refs for buttons to trigger modals
   const btnRef = useRef();
+  const btnRef2 = useRef();
 
+  // Function to handle category selection
   const handleCategoryClick = (category, child) => {
     const newCategory = child
       ? {
@@ -43,9 +50,138 @@ function Searchbar() {
     setSelectedCategory({ ...selectedCategory, ...newCategory });
     onClose();
   };
+
+  // Function to handle city selection
+  const handleCitySelection = (city) => {
+      setSelectedCity(city);
+      setFilteredCities(Cities)
+      onClose2();
+  };
+
+  // JSX for category selection modal
+  const categorySelector = (
+    <Drawer
+      isOpen={isOpen}
+      placement="right"
+      onClose={onClose}
+      finalFocusRef={btnRef}
+      size={"sm"}
+    >
+      <DrawerOverlay />
+      <DrawerContent>
+        <DrawerCloseButton />
+        <DrawerHeader borderBottomWidth={1}>Select a category</DrawerHeader>
+        <DrawerBody>
+          {Categories.map((category) =>
+            category.childerns ? (
+              <Accordion allowToggle key={category.id}>
+                <AccordionItem>
+                  <h2>
+                    <AccordionButton w={"100%"} justifyContent={"start"}>
+                      <div className="flex items-center gap-2 font-semibold">
+                        <a style={{ color: `${category.color}` }} className="text-xl pr-2 ">
+                          {category.icon}
+                        </a>
+                        {category.name}
+                      </div>
+                      <AccordionIcon />
+                    </AccordionButton>
+                  </h2>
+                  <AccordionPanel>
+                    {category.childerns.map((child) => (
+                      <Button
+                        bg={"white"}
+                        padding={"10px 20px"}
+                        justifyContent={"start"}
+                        w={"100%"}
+                        onClick={() => handleCategoryClick(category, child)}
+                        leftIcon={
+                          <a style={{ color: `${category.color}` }} className="text-xl pr-2">
+                            {child.icon}
+                          </a>
+                        }
+                        key={child.id}
+                      >
+                        {child.name}
+                      </Button>
+                    ))}
+                  </AccordionPanel>
+                </AccordionItem>
+              </Accordion>
+            ) : (
+              <Button
+                w={"100%"}
+                justifyContent={"start"}
+                bg={"white"}
+                onClick={() => handleCategoryClick(category)}
+                leftIcon={
+                  <a style={{ color: `${category.color}` }} className="text-xl pr-2">
+                    {category.icon}
+                  </a>
+                }
+                key={category.id}
+              >
+                {category.name}
+              </Button>
+            )
+          )}
+        </DrawerBody>
+      </DrawerContent>
+    </Drawer>
+  );
+
+  // JSX for city selection modal
+  const citySelector = (
+    <Drawer
+      isOpen={isOpen2}
+      placement="right"
+      onClose={onClose2}
+      finalFocusRef={btnRef2}
+    >
+      <DrawerOverlay />
+      <DrawerContent>
+        <DrawerCloseButton />
+        <DrawerHeader>Select City</DrawerHeader>
+
+        <DrawerBody>
+          <InputGroup>
+            <InputLeftElement pointerEvents="none">
+              <CiSearch />
+            </InputLeftElement>
+            <Input
+              type="tel"
+              placeholder="Search City"
+              borderRadius={40}
+              onChange={(e) => {
+                const searchValue = e.target.value.toLowerCase();
+                const filteredCities = Cities.filter((city) =>
+                  city.toLowerCase().startsWith(searchValue)
+                );
+                setFilteredCities(filteredCities);
+              }}
+            />
+          </InputGroup>
+          {filteredCities.map((city) => (
+            <Button
+              onClick={() => handleCitySelection(city)}
+              py={"5px"}
+              pl={"5px"}
+              w={"100%"}
+              key={city}
+              bg={"white"}
+            >
+              {city}
+            </Button>
+          ))}
+        </DrawerBody>
+      </DrawerContent>
+    </Drawer>
+  );
+
   return (
     <div className="w-full shadow-md ">
       <div className="w-full md:px-60 md:flex items-center gap-2 justify-between py-4">
+        {/* Input field for search */}
         <div className="md:w-1/4 h-full md:px-2">
           <InputGroup>
             <Input
@@ -56,16 +192,14 @@ function Searchbar() {
             />
           </InputGroup>
         </div>
+        {/* Button for selecting category */}
         <div className="md:w-1/4 h-full py-2 md:py-0 MD:px-2">
           <Button
             onClick={onOpen}
             _hover={{ bg: "#24lorCC" }}
             rightIcon={<MdNavigateNext className="text-2xl" />}
             leftIcon={
-              <div
-                style={{ color: `${selectedCategory.color}` }}
-                className="text-2xl"
-              >
+              <div style={{ color: `${selectedCategory.color}` }} className="text-2xl">
                 {selectedCategory.icon}
               </div>
             }
@@ -76,25 +210,23 @@ function Searchbar() {
             {selectedCategory.name}
           </Button>
         </div>
+        {/* Button for selecting city */}
         <div className="md:w-1/4 h-full  md:px-2">
-          <InputGroup>
-            <InputLeftElement>
-              <a className="text-gray-600">
-                {" "}
-                <FaMapPin />
-              </a>
-            </InputLeftElement>
-            <Input bg={"#FAFAFA"} type="item" placeholder="all of germany" />
-          </InputGroup>
+          <Button
+            onClick={onOpen2}
+            justifyContent={"space-between"}
+            w={"100%"}
+            leftIcon={<FaTreeCity />}
+            rightIcon={<MdNavigateNext className="text-2xl" />}
+          >
+            {selectedCity}
+          </Button>
         </div>
+        {/* Button for search action */}
         <div className="md:w-1/4 h-full md:px-2 max-sm:mt-5">
           <Button
             _hover={{ bg: "#24s4lorCC" }}
-            leftIcon={
-              <div className="text-2xl">
-                <CiSearch />
-              </div>
-            }
+            leftIcon={<CiSearch className="text-2xl" />}
             color={"white"}
             borderRightRadius={40}
             width={"100%"}
@@ -104,88 +236,9 @@ function Searchbar() {
           </Button>
         </div>
       </div>
-      <Drawer
-        isOpen={isOpen}
-        placement="right"
-        onClose={onClose}
-        finalFocusRef={btnRef}
-        size={"sm"}
-      >
-        <DrawerOverlay />
-        <DrawerContent>
-          <DrawerCloseButton />
-          <DrawerHeader borderBottomWidth={1}>Select a category</DrawerHeader>
-          <DrawerBody>
-            {Categories.map((category) =>
-              category.childerns ? (
-                <Accordion allowToggle>
-                  <AccordionItem>
-                    <h2>
-                      <AccordionButton
-                        w={"100%"}
-                        justifyContent={"start"}
-                        key={category.id}
-                      >
-                        <div className="flex items-center gap-2 font-semibold">
-                          <a
-                            style={{ color: `${category.color}` }}
-                            className="text-xl pr-2 "
-                          >
-                            {category.icon}
-                          </a>
-                          {category.name}
-                        </div>
-                        <AccordionIcon />
-                      </AccordionButton>
-                    </h2>
-                    <AccordionPanel>
-                      {category.childerns.map((child) => (
-                        <Button
-                          bg={"white"}
-                          padding={"10px 20px"}
-                          justifyContent={"start"}
-                          w={"100%"}
-                          onClick={() => handleCategoryClick(category, child)}
-                          leftIcon={
-                            <a
-                              style={{ color: `${category.color}` }}
-                              className="text-xl pr-2"
-                            >
-                              {child.icon}
-                            </a>
-                          }
-                          color
-                          key={child.id}
-                        >
-                          {child.name}
-                        </Button>
-                      ))}
-                    </AccordionPanel>
-                  </AccordionItem>
-                </Accordion>
-              ) : (
-                <Button
-                  w={"100%"}
-                  justifyContent={"start"}
-                  bg={"white"}
-                  onClick={() => handleCategoryClick(category)}
-                  leftIcon={
-                    <a
-                      style={{ color: `${category.color}` }}
-                      className="text-xl pr-2 "
-                    >
-                      {category.icon}
-                    </a>
-                  }
-                  key={category.id}
-                >
-                  {category.name}
-                </Button>
-              )
-            )}
-          </DrawerBody>
-        </DrawerContent>
-      </Drawer>
+      {/* Render modals */}
+      {categorySelector}
+      {citySelector}
     </div>
   );
 }
